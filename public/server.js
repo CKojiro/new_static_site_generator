3c0976c591d501fb98186d7b57a86f6845d66222
CKojiro <smiths12324@gmail.com>
2026-09-24 22:53:32 -0700

    Commit #6: Added server.js as an extra option for running an http server.

---
+++ public/server.js
@@ -0,0 +1,48 @@
+
+const http = require('http');
+
+const PORT = 8888;
+
+const server = http.createServer((req, res) =>
+{
+    if (req.url === '/')
+    {
+        fs.readFile('./index.html', (err, data) =>
+        {
+            if (err)
+            {
+                res.writeHead(500);
+                res.end('Error loading HTML file');
+                return;
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }
+    else if (req.url === '/styles.css')
+    {
+        fs.readFile('./styles.css')
+        {
+            if (err)
+            {
+                res.writeHead(500);
+                res.end('Error loading CSS file');
+                return;
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.end(data);
+        }
+    }
+    else
+    {
+        res.writeHead(404);
+        res.end('Not found');
+    }
+});
+
+server.listen(PORT, () =>
+{
+    console.log(`Server is running at http://localhost:${PORT}/`);
+});
